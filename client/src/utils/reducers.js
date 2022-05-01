4653938facce65d72bafd9cb561b8427da86fb44
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-05-01 14:50:48 -0400

    added cart template and styles

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -67,6 +67,19 @@ export const reducer = (state, action) => {
           return product;
         })
       };
+      
+      case CLEAR_CART:
+        return{
+          ...state,
+          cartOpen: false,
+          cart: []
+        };
+
+      case TOGGLE_CART:
+        return {
+          ...state,
+          cartOpen: !state.cartOpen
+        };
 
 
     default:
